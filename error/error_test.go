f84cae3bac7d7d46736fe74668831eed67e33e71
leimeng <leimeng@qhgctech.com>
2021-05-22 16:44:41 +0800

    updatecode

--- error/error_test.go
+++ error/error_test.go
@@ -29,6 +29,16 @@ func TestMyError(t *testing.T){
 	t.Log(e.Error())
 }
 
+
+// Wrap 方法用来包装底层错误，增加上下文文本信息并添加调用栈。一般用于包装对第三方代码(标准库或者第三方库)的调用
+// innnerError 内部错误
 func innerError()error{
    return gerrors.Wrap(sql.ErrNoRows,"inner")
+}
+
+
+// WithMessage 方法仅添加上下文文本信息，不附加调用栈。如果确定错误已被Wrap过或不关心调用栈，可以使用次方法。
+// outError 外部错误
+func outError()error{
+   return gerrors.WithMessage(innerError(),"outError failed")
 }
